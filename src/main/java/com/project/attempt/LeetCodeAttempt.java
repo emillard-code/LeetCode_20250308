a14b41d90edb7a94c7cd83e59259ba168b4d5870
Emerson Millard <emillard.code@gmail.com>
2025-03-18 12:00:00 -0700

    Added challenge attempt.

--- src/main/java/com/project/attempt/LeetCodeAttempt.java
+++ src/main/java/com/project/attempt/LeetCodeAttempt.java
@@ -4,6 +4,33 @@ public class LeetCodeAttempt {
 
     public static void main(String[] args) {
 
+        System.out.println(minimumRecolorsToGetKConsecutiveBlackBlocks("WBBWWBBWBW", 7));
+
+        System.out.println(minimumRecolorsToGetKConsecutiveBlackBlocks("WBWBBBW", 2));
+
+    }
+
+    public static int minimumRecolorsToGetKConsecutiveBlackBlocks(String blocks, int k) {
+
+        int minimumRecolors = blocks.length();
+
+        for (int i = 0; i <= blocks.length() - k; i++) {
+
+            int requiredRecolors = 0;
+
+            for (int j = i; j < i + k; j++) {
+
+                if (blocks.charAt(j) == 'W') { requiredRecolors++; }
+
+            }
+
+            if (requiredRecolors < minimumRecolors) { minimumRecolors = requiredRecolors; }
+
+        }
+
+        return minimumRecolors;
+
+
     }
 
 }
